6be8b2b3a43e4f19ee1f9d49a6ade2ed295f8e7a
Pradeep S <pradeep.s1@cerner.com>
2018-11-16 15:31:00 +0530

    Removed unused import

--- opennms-services/src/main/java/org/opennms/netmgt/threshd/ThresholdEvaluatorRearmingAbsoluteChange.java
+++ opennms-services/src/main/java/org/opennms/netmgt/threshd/ThresholdEvaluatorRearmingAbsoluteChange.java
@@ -34,7 +34,6 @@ import java.util.Map;
 
 import org.opennms.netmgt.config.threshd.ThresholdType;
 import org.opennms.netmgt.events.api.EventConstants;
-import org.opennms.netmgt.threshd.ThresholdEvaluatorState.Status;
 import org.opennms.netmgt.xml.event.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
